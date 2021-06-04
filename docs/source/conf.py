fc95265307133100e755a1e994492a1e82f1c04d
Yichen <31248391+whoiszyc@users.noreply.github.com>
2021-06-04 17:08:23 -0500

    update docs conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,8 @@ author = 'Yichen Zhang'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser"]
+extensions = ['myst_parser']
+source_suffix = ['.rst', '.md']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
