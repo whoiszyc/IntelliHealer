27e9d00566ec1c19d9e7d5645abda517aca73ffe
Yichen <31248391+whoiszyc@users.noreply.github.com>
2021-06-07 16:20:35 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,3 +4,8 @@ setup(name='gym_power_res',
       version='0.0.1',
       install_requires=['gym']  # And any other dependencies foo needs
 )
+
+setup(name='project_dis_restoration',
+      version='0.0.1',
+      install_requires=['gym']  # And any other dependencies foo needs
+)
